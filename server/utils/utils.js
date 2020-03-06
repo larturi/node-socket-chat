871bff74d7e2d4025b742b78ad81bc4d3661a42b
larturi <lea.arturi@gmail.com>
2020-03-05 21:22:39 -0300

    Primer commit

---
+++ server/utils/utils.js
@@ -0,0 +1,13 @@
+const crearMensaje = (nombre, mensaje) => {
+
+    return {
+        nombre: nombre,
+        mensaje: mensaje,
+        fecha: new Date().getTime()
+    };
+
+}
+
+module.exports = {
+    crearMensaje
+}
